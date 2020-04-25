e6fa80566d346038768263be0f5caeb52af56687
pauline <monteilpauline65@gmail.com>
2020-04-25 12:42:36 +0200

    Routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Movies;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/movie', function (Request $request) {
+    return Movies::get();
+});
+//Route qui redirige soit vers un films 
+Route::get('/movie/{n}', function ($n) {
+    if ($n === "last") return Movies::get()->sortByDesc('created_at')->first();
+    if (intval($n)) return Movies::get()->where('id', intval($n));
+    return null;
 });
